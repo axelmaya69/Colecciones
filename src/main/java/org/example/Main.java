f05ea524fe47894425275df60ba410a5faab7fac
axelmaya69 <axeldbmaya96@gmail.com>
2024-08-22 15:23:23 -0600

    message

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,28 +7,26 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         List<Persona> lista = new ArrayList<>(); //asi se declara un arrayList
-    lista.add(new Persona(1,"Juan",22));
-        lista.add(new Persona(2,"Maria",12));
-        lista.add(new Persona(3,"Marcos",24)); //llenado de la lista
-        lista.add(new Persona(4,"Luis",43));   //dinamica
+        lista.add(new Persona(1, "Juan", 22));
+        lista.add(new Persona(2, "Maria", 12));
+        lista.add(new Persona(3, "Marcos", 24)); //llenado de la lista
+        lista.add(new Persona(4, "Luis", 43));   //dinamica
 
         //recorrer por indice
         for (int i = 0; i < lista.size(); i++) {
-            System.out.println("Prueba "+lista.get(i).getNombre());
+            System.out.println("Prueba " + lista.get(i).getNombre());
         }
 
         //recorrido forEach
-        for (Persona pipol:lista){
-            System.out.println("Prueba ForEach: "+ pipol.getNombre());
+        for (Persona pipol : lista) {
+            System.out.println("Prueba ForEach: " + pipol.getNombre());
         }
 
         //eliminar un elemento
         lista.remove(1);
         for (int i = 0; i < lista.size(); i++) {
-            System.out.println("Elementos actualizados: "+lista.get(i).getNombre());
+            System.out.println("Elementos actualizados: " + lista.get(i).getNombre());
         }
-        
     }
-    
     
 }
