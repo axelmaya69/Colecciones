c4ac467b07d0b887c30230fa0bb0ce9e09c39d03
axelmaya69 <axeldbmaya96@gmail.com>
2024-08-22 15:30:21 -0600

    Revert "implementacion de linkedlist"
    This reverts commit 40b94e58910c393577ce9a6cef3af914addaaa91.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,16 +6,16 @@ import java.util.*;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        List<Persona> lista = new LinkedList<Persona>(); //asi se declara un linkedList
-
+        List<Persona> lista = new ArrayList<>(); //asi se declara un arrayList
     lista.add(new Persona(1,"Juan",22));
         lista.add(new Persona(2,"Maria",12));
         lista.add(new Persona(3,"Marcos",24)); //llenado de la lista
         lista.add(new Persona(4,"Luis",43));   //dinamica
 
-        //agregando al inicio
-        lista.add(0,new Persona(1,"chucin",21));
+        //recorrer por indice
+        for (int i = 0; i < lista.size(); i++) {
+            System.out.println("Prueba "+lista.get(i).getNombre());
+        }
 
         //recorrido forEach
         for (Persona pipol:lista){
@@ -27,7 +27,7 @@ public class Main {
         for (int i = 0; i < lista.size(); i++) {
             System.out.println("Elementos actualizados: "+lista.get(i).getNombre());
         }
-
+        
     }
     
     
